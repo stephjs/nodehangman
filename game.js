583bf2876bf1ab251b560585ab5078897f623c0e
stephorpilla <stephanieorpilla@yahoo.com>
2016-10-30 16:21:37 -0700

    fixed word list bug

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var list = var wordsList = ["ironic", "filter", "chella", "brooklyn", "barrista", "liberal", "basic", "thundercats", "ugh", "gentrify", "juicing", "kitsch", "kogi", "biodiesel", "chartreuse", "skateboard", "artisan", "pitchfork", "organic", "ipa", "ethical", "chambray", "overexposed", "hashtag", "umami", "kale", "sriracha", "affogato", "brunch", "aesthetic", "banjo", "distillery", "chill", "vegan", "typewriter", "quinoa", "tumblr", "hammock", "dreamcatcher", "beard", "cronut", "emoji", "moustache", "unicycle"];
+var list = ["ironic", "filter", "chella", "brooklyn", "barrista", "liberal", "basic", "thundercats", "ugh", "gentrify", "juicing", "kitsch", "kogi", "biodiesel", "chartreuse", "skateboard", "artisan", "pitchfork", "organic", "ipa", "ethical", "chambray", "overexposed", "hashtag", "umami", "kale", "sriracha", "affogato", "brunch", "aesthetic", "banjo", "distillery", "chill", "vegan", "typewriter", "quinoa", "tumblr", "hammock", "dreamcatcher", "beard", "cronut", "emoji", "moustache", "unicycle"];
 
 var index = Math.floor(Math.random()*(list.length));
 var mysteryWord = list[index];
